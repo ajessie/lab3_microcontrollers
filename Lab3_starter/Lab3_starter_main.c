bba07776aca4e987bd9bf1ce727deb5e7f1fe7b1
Jessie Acfalle <ajessie@vt.edu>
2018-11-03 23:31:20 -0400

    updated code for lab3

--- Lab3_starter/Lab3_starter_main.c
+++ Lab3_starter/Lab3_starter_main.c
@@ -13,19 +13,56 @@ extern HWTimer_t timer0, timer1;
 // This function initializes all the peripherals except graphics
 void initialize();
 void ModifyLEDColor(bool leftButtonWasPushed, bool rightButtonWasPushed);
+void Splash();
 
 #define LEFT_THRESHOLD  0x1000
 #define DURATION 100
+#define THREE_SEC 40
+#define ONE_SEC 48000000
 
 extern song_t enter_sandman;
 extern song_t hokie_fight;
+Graphics_Context g_sContext;
 
 int main(void)
 {
+    int three_count = 0;
+
     Graphics_Context g_sContext;
+
     InitGraphics(&g_sContext);
 
     initialize();
+
+    while(1){
+    if (Timer32_getValue(TIMER32_0_BASE == 0))
+        three_count++;
+
+    if(three_count < THREE_SEC){
+    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
+
+    Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_SEA_GREEN);
+
+    char Name[22] = "Jessie Acfalle";
+    Graphics_drawString(&g_sContext, (int8_t *) Name, -1, 10, 64, true);
+    char Game[22] = "Guitar Hero";
+
+    Graphics_drawString(&g_sContext, (int8_t *) Game, -1, 10, 80, true);
+    }
+
+    else if (three_count == THREE_SEC){
+        Graphics_clearDisplay(&g_sContext);
+        Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_BLUE);
+        Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
+        char Title[22] = ">How to Play";
+        Graphics_drawString(&g_sContext, (int8_t *) Title, -1, 10, 64, true);
+        char Play[22]  = "Lets Rock!";
+        Graphics_drawString(&g_sContext, (int8_t *) Play, -1, 10, 80, true);
+    }
+ }
+
+
+
 
     button_t LauchpadLeftButton;
     initButton(&LauchpadLeftButton, GPIO_PORT_P1, GPIO_PIN1, &timer0);
@@ -33,7 +70,7 @@ int main(void)
     button_t LauchpadRightButton;
     initButton(&LauchpadRightButton, GPIO_PORT_P1, GPIO_PIN4, &timer0);
 
-    draw_Base(&g_sContext);
+//    draw_Base(&g_sContext);
     unsigned vx, vy;
 
     //TODO: comment out this part once you complete part 3
@@ -45,39 +82,39 @@ int main(void)
 */
 
 
-    while (1)
-    {
-        bool leftButtonPushed = ButtonPushed(&LauchpadLeftButton);
-        bool rightButtonPushed = ButtonPushed(&LauchpadRightButton);
-
-        if (LaunchpadLeftButton_pressed())
-            turnOn_LaunchpadLED1();
-        else
-            turnOff_LaunchpadLED1();
-
-        if (LaunchpadRightButton_pressed())
-            turnOn_LaunchpadLED2_red();
-        else
-            turnOff_LaunchpadLED2_red();
-
-
-        ModifyLEDColor(leftButtonPushed,rightButtonPushed);
-        MoveCircle(&g_sContext, leftButtonPushed,rightButtonPushed);
-
-        getSampleJoyStick(&vx, &vy);
-        bool joyStickPushedtoRight = false;
-        bool joyStickPushedtoLeft = false;
-        drawXY(&g_sContext, vx, vy);
-
-        if (vx < LEFT_THRESHOLD)
-        {
-            joyStickPushedtoLeft = true;
-        }
-
-        ModifyLEDColor(joyStickPushedtoLeft,joyStickPushedtoRight);
-        MoveCircle(&g_sContext, joyStickPushedtoLeft,joyStickPushedtoRight);
-
-    }
+//    while (1)
+//    {
+//        bool leftButtonPushed = ButtonPushed(&LauchpadLeftButton);
+//        bool rightButtonPushed = ButtonPushed(&LauchpadRightButton);
+//
+//        if (LaunchpadLeftButton_pressed())
+//            turnOn_LaunchpadLED1();
+//        else
+//            turnOff_LaunchpadLED1();
+//
+//        if (LaunchpadRightButton_pressed())
+//            turnOn_LaunchpadLED2_red();
+//        else
+//            turnOff_LaunchpadLED2_red();
+//
+//
+//        ModifyLEDColor(leftButtonPushed,rightButtonPushed);
+//        MoveCircle(&g_sContext, leftButtonPushed,rightButtonPushed);
+//
+//        getSampleJoyStick(&vx, &vy);
+//        bool joyStickPushedtoRight = false;
+//        bool joyStickPushedtoLeft = false;
+//        drawXY(&g_sContext, vx, vy);
+//
+//        if (vx < LEFT_THRESHOLD)
+//        {
+//            joyStickPushedtoLeft = true;
+//        }
+//
+//        ModifyLEDColor(joyStickPushedtoLeft,joyStickPushedtoRight);
+//        MoveCircle(&g_sContext, joyStickPushedtoLeft,joyStickPushedtoRight);
+//
+//    }
 }
 
 
@@ -85,6 +122,13 @@ void initialize()
 {
     // stop the watchdog timer
     WDT_A_hold(WDT_A_BASE);
+
+    Timer32_initModule(TIMER32_0_BASE, // There are two timers, we are using the one with the index 0
+                       TIMER32_PRESCALER_1, // The prescaler value is 1; The clock is not divided before feeding the counter
+                       TIMER32_32BIT, // The counter is used in 32-bit mode; the alternative is 16-bit mode
+                       TIMER32_PERIODIC_MODE); //This options is irrelevant for a one-sh
+    Timer32_setCount(TIMER32_0_BASE, ONE_SEC);  // The count related to the period of the timer
+    Timer32_startTimer(TIMER32_0_BASE, false);
 
     // initialize the boosterPack LEDs and turn them off except for red LED
     initialize_BoosterpackLED_red();
@@ -110,6 +154,16 @@ void initialize()
     initJoyStick();
     startADC();
 }
+
+//void Splash(){
+//    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
+//
+//    Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_SEA_GREEN);
+//
+//    char Name[22] = "Jessie Acfalle";
+//    Graphics_drawString(&g_sContext, (int8_t *) Name, -1, 1, 100, true);
+//
+//}
 
 // This FSM has two inputs each of them the FSM if a button has been pushed or not
 // The FSM has three states: Red, Green, Blue. The initial state is Red
